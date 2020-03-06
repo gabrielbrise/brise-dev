5d058a500b6eed2c367a18d80b375391ed99a3fa
Brise <gabriel_ns@live.com>
2020-03-05 22:01:47 -0300

    server side rendering styled-components

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,63 +1,83 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled from "styled-components";
 
-const AboutMe = () => (
-  <div className="container-fluid nav-margin">
-    <div className="row me_body">
-      <div className="row bio-intro">
-        <div className="about-me col-md-8 col-lg-9 col-sm-12 d-flex align-items-center">
-          <div className="jumbotron about-me align-middle">
-            <h1 className="display-4 hi fadein">
-              Olá! Você pode me chamar de BRISE!
-            </h1>
-            <br />
+const AboutMe = () => {
+  return (
+    <Container className="container-fluid nav-margin">
+      <div className="row me_body">
+        <div className="row bio-intro">
+          <div className="about-me col-md-8 col-lg-9 col-sm-12 d-flex align-items-center">
+            <div className="jumbotron about-me align-middle">
+              <h1 className="display-4 hi FadeIn">
+                Olá! Você pode me chamar de BRISE!
+              </h1>
+              <br />
 
-            <p
-              className="lead intro"
-              text-align="center"
-              style={{ padding: "0 10% 0 10%" }}
-            >
-              Me chamo Gabriel, tenho 25 anos, sou recém formado em Arquitetura
-              e Urbanismo pela UFRGS e estou interessado em explorar áreas da
-              tecnologia onde meus conhecimentos adquiridos ao longo da
-              faculdade possam ser aplicados.
-              <br />
-              <br />
-              Ganhei o apelido Brise devido ao primeiro trabalho que apresentei
-              na faculdade, onde expliquei o que são os brises (elementos
-              arquitetônicos para proteção solar).
-              <br />
-              <br />
-              Atualmente estou fazendo cursos ligados à programação (HTML/CSS,
-              python/kivy, C#/Unity e Javascript/React) e gostaria de encontrar
-              um ambiente de trabalho para colaborar aplicando o que aprendo.
-              Tenho facilidade em resolução de problemas e isso me permite
-              aprender novas ferramentas rapidamente.
-              <br /> <br />
-              Espero que goste dessa página que fiz para exemplificar algumas de
-              minhas habilidades!
-            </p>
+              <p
+                className="lead FadeIn"
+                text-align="center"
+                style={{ padding: "0 10% 0 10%" }}
+              >
+                Me chamo Gabriel, tenho 25 anos, sou recém formado em
+                Arquitetura e Urbanismo pela UFRGS e estou interessado em
+                explorar áreas da tecnologia onde meus conhecimentos adquiridos
+                ao longo da faculdade possam ser aplicados.
+                <br />
+                <br />
+                Ganhei o apelido Brise devido ao primeiro trabalho que
+                apresentei na faculdade, onde expliquei o que são os brises
+                (elementos arquitetônicos para proteção solar).
+                <br />
+                <br />
+                Atualmente estou fazendo cursos ligados à programação (HTML/CSS,
+                python/kivy, C#/Unity e Javascript/React) e gostaria de
+                encontrar um ambiente de trabalho para colaborar aplicando o que
+                aprendo. Tenho facilidade em resolução de problemas e isso me
+                permite aprender novas ferramentas rapidamente.
+                <br /> <br />
+                Espero que goste dessa página que fiz para exemplificar algumas
+                de minhas habilidades!
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div className="me-body col-md-4 col-lg-3 d-none d-sm-flex d-lg-flex">
-          <div className="d-flex align-items-end fadein">
+          <div className="me-body col-md-4 col-lg-3 d-none d-sm-flex d-lg-flex">
+            <div className="d-flex align-items-end">
+              <img
+                src="img/me_body.jpg"
+                className="img-fluid me-body FadeIn"
+                alt="Responsive image"
+              />
+            </div>
+          </div>
+          <div className="me-small d-block d-sm-none">
             <img
-              src="img/me_body.jpg"
-              className="img-fluid me-body"
+              src="img/me_small.png"
+              className="img-fluid rounded-circle align-middle"
               alt="Responsive image"
             />
           </div>
         </div>
-        <div className="me-small d-block d-sm-none">
-          <img
-            src="img/me_small.png"
-            className="img-fluid rounded-circle align-middle"
-            alt="Responsive image"
-          />
-        </div>
       </div>
-    </div>
-  </div>
-);
+    </Container>
+  );
+};
 
 export default AboutMe;
+
+const Container = styled.section`
+  background-color: #e5e4ea;
+  .FadeIn {
+    @keyframes fadein {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+    animation-duration: 3s;
+    animation-name: fadein;
+    animation-iteration-count: 1;
+  }
+`;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
@@ -12,34 +32,30 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="shortcut icon" href="/img/favicon.ico" />
+          <link rel="shortcut icon" href="img/favicon.ico"></link>
           <link
             rel="stylesheet"
             type="text/css"
-            href="/css/bootstrap.min.css"
-          />
-          <script language="javascript" src="/teste.js"></script>
+            href="css/bootstrap.min.css"
+          ></link>
           <link
             rel="stylesheet"
             type="text/css"
-            href="/css/bootstrap-theme.min.css"
-          />
-          <link rel="stylesheet" type="text/css" href="css/style.css" />
-          <link href="/css/all.css" rel="stylesheet" />
+            href="css/bootstrap-theme.min.css"
+          ></link>
+          <link rel="stylesheet" type="text/css" href="css/style.css"></link>
+          <link href="css/all.css" rel="stylesheet"></link>
           <link
             rel="stylesheet"
             href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
             integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
             crossOrigin="anonymous"
-          />
-          <script type="text/javascript" src="/js/jquery.js"></script>
-          <script type="text/javascript" src="/js/anim_jquery.js"></script>
-
+          ></link>
           <link
             rel="stylesheet"
             type="text/css"
             href="https://www.visualcv.com/less/monte?%40header-font=rasa"
-          />
+          ></link>
         </Head>
         <body>
           <Main />
@@ -49,5 +65,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
