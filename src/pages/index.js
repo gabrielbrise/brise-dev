769a5f5a4f9abb4485a7db909459fc1d4c33af68
Brise <gabriel_ns@live.com>
2020-03-05 21:23:39 -0300

    remove unused scripts

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,6 @@ function HomePage() {
       <Curriculum {...curriculum} />
       <footer className="fixed-bottom webfix">
         <div>Criado por Brise</div>
-        <script src="/js/jquery.js" type="text/javascript"></script>
-        <script src="/js/bootstrap.min.js" type="text/javascript"></script>
       </footer>
     </>
   );
