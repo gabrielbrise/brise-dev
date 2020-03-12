ba1fdbfe8b6797cd341a3d96e96c59ae8f8d77a5
Brise <gabriel_ns@live.com>
2020-03-11 23:38:15 -0300

    software page

---
+++ src/pages/software.js
@@ -0,0 +1,24 @@
+import NavBar from "../components/NavBar.js";
+import CardSection from "../components/CardSection";
+
+import { cardSection } from "../data/software.json";
+
+function HomePage() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <CardSection {...cardSection} />
+
+      <footer className="fixed-bottom webfix">
+        <div>Criado por Brise</div>
+        <script src="js/jquery.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="js/anim_jquery.js"></script>
+      </footer>
+    </>
+  );
+}
+
+export default HomePage;
