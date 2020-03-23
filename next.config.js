aef99abc4ecbb01e25c2c2549d32bac44e887d64
Brise <gabriel_ns@live.com>
2020-03-22 21:49:03 -0300

    add next config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  exportPathMap: function() {
+    return {
+      "/": { page: "/" },
+      "/software": { page: "software" },
+      "/architecture": { page: "architecture" },
+      "/art": { page: "art" },
+      "/ui-ux": { page: "ui-ux" },
+      "/game": { page: "game" }
+    };
+  }
+};
