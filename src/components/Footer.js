152742222eae2d5cc45cc8fc6ee36b9f30f197e6
Brise <gabriel_ns@live.com>
2020-03-24 00:12:54 -0300

    remove footer visuals

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="fixed-bottom webfix">
-    <div>Criado por Brise</div>
+  <footer>
     <script src="js/jquery.js" type="text/javascript"></script>
     <script src="js/bootstrap.min.js" type="text/javascript"></script>
   </footer>
